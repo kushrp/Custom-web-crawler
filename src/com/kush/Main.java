e92df80ed85c6cd9e4fe1c2ca308edb964489854
Kush Rustagi <krustagi@purdue.edu>
2016-09-11 16:36:58 -0400

    Crawler works with unique links

--- src/com/kush/Main.java
+++ src/com/kush/Main.java
@@ -20,10 +20,8 @@ public class Main {
         //store the URL to HashTable to avoid parsing again
 
         //get useful information
-        Document doc = Jsoup.connect(URL).get();
 
         //List links approach
-
 
         /*
         String title = doc.title();
@@ -46,13 +44,28 @@ public class Main {
                 processPage(link.attr("abs:href"));
         }
         */
-        Hashtable words = new Hashtable();
         Queue<String> queue = new LinkedList<>();
-        Elements links = doc.select("a[href]");
-        print("\nLinks: (%d)", links.size());
-        for (Element link : links) {
-            queue.add(link.attr("abs:href"));
-            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 100));
+        queue.add(URL);
+        Hashtable<String, Integer> urls = new Hashtable<>();
+        urls.put(URL,1);
+        while (queue.isEmpty() == false && queue.size() <= 1000) {
+            Document doc = Jsoup.connect(queue.poll()).timeout(0).get();
+            Elements links = doc.select("a[href]");
+            print("\nLinks: (%d)", links.size());
+            for (Element link : links) {
+                //System.out.println("HI");
+                if (urls.containsKey(link.attr("abs:href")) == false) {
+                    //System.out.println("HI");
+                    queue.add(link.attr("abs:href"));
+                    urls.put(link.attr("abs:href"), 1);
+                    print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 100));
+                }
+                else {
+                    //System.out.println("YOLO");
+                    int num = urls.get(link.attr("abs:href"));
+                    urls.put(link.attr("abs:href"), num+1);
+                }
+            }
         }
 
     }
